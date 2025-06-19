258c81de62ced977c5c00432422a6c2ca8c245a3
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-19 20:07:59 +0530

    Update variables.tf

--- modules/cognito/variables.tf
+++ modules/cognito/variables.tf
@@ -13,3 +13,16 @@ variable "domain_prefix" {
 variable "api_endpoint" {
 type = list(string)
 }
+
+variable "default_user_username" {
+  type = string
+}
+
+variable "default_user_email" {
+  type = string
+}
+
+variable "default_user_password" {
+  type = string
+}
+

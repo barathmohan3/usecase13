34fde2e7d3666cece29439bb2815b044831d7442
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-19 19:44:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,5 +18,5 @@ module "cognito" {
   user_pool_name   = var.user_pool_name
   app_client_name  = var.app_client_name
   domain_prefix    = var.domain_prefix
-  callback_urls    = [module.apigateway.api_endpoint]
+  api_endpoint    = [module.apigateway.api_endpoint]
 }

3283bff57b467f6957bdceb154a7805ebbae442c
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-20 13:11:14 +0530

    Update outputs.tf

--- modules/cognito/outputs.tf
+++ modules/cognito/outputs.tf
@@ -9,3 +9,7 @@ output "user_pool_arn" {
 output "client_id" {
   value = aws_cognito_user_pool_client.client.id
 }
+
+output "domain_prefix" {
+  value = aws_cognito_user_pool_domain.domain.domain
+}

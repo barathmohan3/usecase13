d0e92584fd3301d26cfe354efaeb909f69b37f5d
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-20 13:06:26 +0530

    Update variables.tf

--- modules/apigateway/variables.tf
+++ modules/apigateway/variables.tf
@@ -1,3 +1,15 @@
 variable "lambda_function_arn" {
   type = string
 }
+
+variable "cognito_client_id" {
+  type = string
+}
+
+variable "cognito_user_pool_domain" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}

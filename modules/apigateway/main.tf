5220c3a35417638b8730de0ac3af96bebbe484bc
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-20 16:02:00 +0530

    Update main.tf

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -11,6 +11,19 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   payload_format_version  = "2.0"
 }
 
+resource "aws_apigatewayv2_authorizer" "cognito_auth" {
+  api_id          = aws_apigatewayv2_api.http_api.id
+  name            = "CognitoJWTAuthorizer"
+  authorizer_type = "JWT"
+
+  identity_sources = ["$request.header.Authorization"]
+
+  jwt_configuration {
+    audience = [var.client_id] # Pass this from Cognito module output
+    issuer   = "https://${var.domain_prefix}.auth.${var.aws_region}.amazoncognito.com"
+  }
+}
+
 resource "aws_apigatewayv2_route" "default_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "GET /"

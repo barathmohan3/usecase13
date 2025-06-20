cb7813fb99eac9517616c19b218a3dad863e3d58
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-20 16:02:28 +0530

    Update main.tf

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -28,7 +28,10 @@ resource "aws_apigatewayv2_route" "default_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "GET /"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.cognito_auth.id
 }
+
 
 resource "aws_apigatewayv2_stage" "default_stage" {
   api_id      = aws_apigatewayv2_api.http_api.id
